84f261411cd409c1b6257793e4fb67eede1d4d3d
francesco <francescosanti91@gmail.com>
2022-01-25 16:26:30 +0100

    aggiunta migration tabella songs

---
+++ database/migrations/2022_01_25_151618_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('album_id');
+            $table->string('title');
+            $table->unsignedBigInteger('num_of_play');
+            $table->time('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
